a152068e63df0e391eb7fe8914633dd28b72c5fc
lucy <lucy.email@example.com>
2026-07-14 23:29:42 +0900

    api doc add

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from "express";
-
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+
+import swaggerSpec from "./config/swagger.js";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.use(
   cors({
@@ -12,6 +18,9 @@ app.use(
 );
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/", (req, res) => {
   res.json({ message: "Favorite Photo API Server" });
