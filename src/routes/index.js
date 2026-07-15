bf1a1f867be07933341460c7bb50b1d032d6f0a2
singsangsong28 <kuu052888@gmail.com>
2026-07-15 15:52:52 +0900

    feat: 내 정보 조회(GET /users/me) 엔드포인트 구현

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 // src/routes/index.js
 import { Router } from "express";
 import authRouter from "../modules/auth/auth.route.js";
+import userRouter from "../modules/user/user.route.js";
 
 const router = Router();
 
 // 각 모듈 라우터 연결 (구현되는 순서대로 추가)
 router.use("/auth", authRouter);
+router.use("/users", userRouter);
 
 export default router;
