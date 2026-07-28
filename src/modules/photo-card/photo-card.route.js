67234b544663b21c97e42e18cc1959c5b9b11621
lucy <lucy.email@example.com>
2026-07-28 16:12:23 +0900

    fix: import delete

--- src/modules/photo-card/photo-card.route.js
+++ src/modules/photo-card/photo-card.route.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 
-import cloudinary from "../../config/cloudinary.js";
 import { protect } from "../../common/middlewares/auth.js";
 import { uploadImage } from "../../common/middlewares/imageUpload.js";
 import { validate } from "../../common/middlewares/validate.js";
